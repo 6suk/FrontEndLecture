b1699b9cc2220e5770a59f950bd012789d4cef74
6suk <koandjo9116@gmail.com>
2022-12-01 18:28:10 +0900

    JS : Date Upload

---
+++ 05.JavaScript/ch07.표준내장객체/03.Date.js
@@ -0,0 +1,11 @@
+// 현재시간
+let today = new Date();
+let todayStr = Date();
+
+console.log(today.toDateString());
+console.log(today.toLocaleDateString());
+console.log(today.toTimeString());
+
+console.log(today);
+console.log(today.toString());
+console.log(todayStr);
